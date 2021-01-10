0a77b969b2159cae946886b421b1947653e6b84b
iia <kha2464@gmail.com>
2021-01-10 23:26:47 +0900

    2.4 Firebase 회원가입 및 로그인
    - 이메일/패스워드로 Firebase 회원가입 기능 추가
    - 로그인 상태 판단 로직 변경
        as-is : 로그인 상태 판단을 위해 바로 authService.currentUser를 호출
        to-be : onAuthStateChanged API를 통해 계정 상태 판단
    - 회원가입/로그인 실패 시 에러메시지 출력

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Router from "components/Router";
-import {authService} from "myFirebase"
+import {authService} from "myFirebase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(!!authService.currentUser);
-  return <Router isLoggedIn={isLoggedIn} />;
+    const [init, setInit] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // useEffect : component가 mount 될 때 시작되는 hook
+    useEffect(() => {
+        // onAuthStateChanged : 사용자의 로그인 상태의 변화를 관찰하는 firebase event listener
+        // firebase 초기화, 로그아웃, 계정 생성, 로그인 때에도 트리거 발생
+        // https://firebase.google.com/docs/auth/web/manage-users?hl=ko
+        authService.onAuthStateChanged((user) => {
+            console.log(user);
+            if (user) {
+                setIsLoggedIn(true);
+            } else {
+                setIsLoggedIn(false);
+            }
+            setInit(true);
+        });
+    }, []);
+
+    return <>
+        {init ? <Router isLoggedIn={isLoggedIn}/> : "initializing..."}
+    </>;
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {authService} from 'myFirebase';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+
+    const onChange = (e) => {
+        const {target: {name, value}} = e;
+        if (name === 'email') {
+            setEmail(value);
+        }
+        else if (name === 'password') {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            if (newAccount) {
+                await authService.createUserWithEmailAndPassword(email, password);
+            }
+            else {
+                await authService.signInWithEmailAndPassword(email, password);
+            }
+        }
+        catch (error) {
+            setError(error.message);
+        }
+    }
+
+    const toggleAccount = (prev) => {
+        setNewAccount(!prev);
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" value={email} onChange={onChange} type="text" placeholder="Email" required/>
+                <input name="password" value={password} onChange={onChange} type="password" placeholder="Password" required/>
+                <input type="submit" value={newAccount? "회원가입" : "로그인"}/>
+                {error}
+            </form>
+            <span onClick={toggleAccount} value={newAccount ? "회원가입 하기" : "로그인 하기"} />
+            <div>
+                <button>구글아이디로 로그인</button>
+                <button>깃헙아이디로 로그인</button>
+            </div>
+        </div>
+    );
+}
 
 export default Auth;
