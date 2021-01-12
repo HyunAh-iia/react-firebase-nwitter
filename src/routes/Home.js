0b3a14e2151965e1cddfbf0cd5b09c3dd0258cf4
iia <kha2464@gmail.com>
2021-01-13 00:51:53 +0900

    3.2 Cloud Firestore 조회하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,31 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {dbService} from "../myFirebase";
 
 const Home = () => {
     const [tweet, setTweet] = useState("");
+    const [tweets, setTweets] = useState([]);
+    const getTweets = async () => {
+        const dbTweets = await dbService.collection("tweets").get();
+        dbTweets.forEach((doc) => {
+            const docObj = {
+                ...doc.data(),
+                id: doc.id,
+            };
+
+            setTweets((prev) => [docObj, ...prev]);
+        });
+    };
+
+    useEffect(() => {
+        getTweets();
+    }, []);
+
 
     const onSubmit = async (e) => {
         e.preventDefault();
         await dbService.collection("tweets").add({
             tweet,
             createdAt: Date.now(),
-
         });
         setTweet("");
     };
@@ -17,12 +33,20 @@ const Home = () => {
     const onChange = (e) => {
         const {target: {value}} = e;
         setTweet(value);
-    }
-    return <>
+    };
+
+    return <div>
         <form>
             <input type="text" value={tweet} onChange={onChange} placeholder="오늘은 어떠셨나요?" maxLength={120}/>
             <input type="submit" onClick={onSubmit} value="tweet"/>
         </form>
-    </>;
+        <div>
+            {tweets.map((doc) => (
+                <div key={doc.id}>
+                    <h4>{doc.tweet}</h4>
+                </div>
+            ))}
+        </div>
+    </div>;
 }
 export default Home;
