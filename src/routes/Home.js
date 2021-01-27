75722f0bfdf69fffdde445fe8d7eda4ad0b249ab
iia <kha2464@gmail.com>
2021-01-27 23:33:57 +0900

    4.1 Javascript FileReader API로 이미지 첨부 및 프리뷰

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,11 +5,12 @@ import Tweet from "components/Tweet";
 const Home = ({userObj}) => {
     const [tweet, setTweet] = useState("");
     const [tweets, setTweets] = useState([]);
+    const [previewImage, setPreviewImage] = useState();
 
     useEffect(() => {
         dbService.collection("tweets").onSnapshot(snapshot => { //snapshot => QuerySnapshot
             //snapshot.docs => QueryDocumentSnapshot
-            const tweetArray = snapshot.docs.map( doc=> ({
+            const tweetArray = snapshot.docs.map(doc => ({
                 id: doc.id,
                 ...doc.data(), // QueryDocumentSnapshot.data()
             }));
@@ -33,14 +34,40 @@ const Home = ({userObj}) => {
         setTweet(value);
     };
 
+    const onFileChange = (e) => {
+        const {target: {files}} = e;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            console.log(finishedEvent); // readAsDataURL()가 끝나면, 즉 파일을 읽은 후 트리거 발생
+            const {currentTarget: {result}} = finishedEvent;
+            setPreviewImage(result);
+        };
+        reader.readAsDataURL(theFile); // 이미지를 URL 형태로 인코딩 (브라우저를 통해 디코딩하면 이미지로 표시 가능)
+    };
+
+    const onClearImage = (e) => {
+        setPreviewImage(null);
+    }
+
     return <div>
         <form>
-            <input type="text" value={tweet} onChange={onChange} placeholder="오늘은 어떠셨나요?" maxLength={120}/>
-            <input type="submit" onClick={onSubmit} value="tweet"/>
+            <input type="text"
+                   value={tweet}
+                   onChange={onChange}
+                   placeholder="오늘은 어떠셨나요?"
+                   maxLength={120} />
+            <input type="file" accept="image/*" onChange={onFileChange} />
+            <input type="submit" onClick={onSubmit} value="tweet" />
+            {previewImage &&
+            <div>
+                <img src={previewImage} width="50px" height="50px"/>
+                <button onClick={onClearImage}>취소</button>
+            </div>}
         </form>
         <div>
             {tweets.map((doc) => (
-                <Tweet key={doc.id} tweetObj={doc} isOwner={doc.userId === userObj.uid} />
+                <Tweet key={doc.id} tweetObj={doc} isOwner={doc.userId === userObj.uid}/>
             ))}
         </div>
     </div>;
